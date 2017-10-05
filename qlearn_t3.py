0cccac11655f800457332a0fecf6b17d88a912b0
Brian Buxton <bbuxton@umd.edu>
2017-10-04 17:30:20 -0700

    Small change for testing different learning strategies

--- qlearn_t3.py
+++ qlearn_t3.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 """ module for running qlearn tests on tic tac toe environments """
 import t3
+import time
 from qtable_trainer import QTableTrainer
+from qnetwork_trainer import QNetworkTrainer
 
+def train(trainer, env):
+    start = time.time()
+    average_cumulative_reward = trainer.learn(env, 2000)
+    print("Score over time: %f" % average_cumulative_reward)
+    print("Training Time: %f" % (time.time() - start))
 
 def main():
     # the tic tac toe environment
     t3env = t3.make()
-    trainer = QTableTrainer(t3env)
-    average_cumulative_reward = trainer.learn(t3env, 2000)
-    print("Score over time: " +  str(average_cumulative_reward))
+    print("Training with QTable...")
+    train(QTableTrainer(t3env), t3env)
+    print("Training with QNetwork...")
+    train(QNetworkTrainer(), t3env)
 
 
 if __name__ == "__main__":
